2290245fc41da1ec19f3ee0014cc5acf42eb22dd
drelonperry <115036180+drelonperry@users.noreply.github.com>
2024-02-21 00:09:49 -0600

    Update test.c
    Fixed Format

--- test.c
+++ test.c
@@ -36,36 +36,36 @@ int main(int argc, char *argv[])
 
 	do
 	{
-	  //Chose the color red for my prompt
+	 	//Chose the color red for my prompt
 		printf("\033[1;31m");
 		printf("%s$ ", getcwd(s,100));
 		fgets(input, 100, stdin);
-    input[strlen(input) - 1] = '\0';
+    		input[strlen(input) - 1] = '\0';
 
 		//Allocated memory for the arguments.
-    for(i = 0; i < 10; i++)
-    {
-	    args[i] = (char*)malloc(20*sizeof(char));
-    }
+    		for(i = 0; i < 10; i++)
+    		{
+	    		args[i] = (char*)malloc(20*sizeof(char));
+    		}
 
 		//Seperating the arguments into tokens and adding them to the arguments array.
-    token = strtok(input," ");
-    strcpy(args[k++], token);
+    		token = strtok(input," ");
+    		strcpy(args[k++], token);
 
-    while(token != NULL)
-    {
-      token = strtok(NULL," ");
+    		while(token != NULL)
+    		{
+      			token = strtok(NULL," ");
 
-      if(token == NULL)
+      			if(token == NULL)
 			{
-        break;
+        			break;
 			}
 
-      strcpy(args[k++], token);
-    }
+      			strcpy(args[k++], token);
+    		}
 
-    args[k] = NULL;
-    pid = fork();
+    		args[k] = NULL;
+    		pid = fork();
 
 		//If statement if the first argument is a built-in command
 		if(strcmp(args[0], "exit") == 0 || strcmp(args[0], "cd") == 0 || strcmp(args[0], "showpid") == 0)
@@ -77,23 +77,22 @@ int main(int argc, char *argv[])
 		{
 			printf("Forking Failed\n");
 		}
-    else if(pid == 0) //Child Process
-    {
-      if(execvp(args[0], args) < 0)
-		  {
-        printf("Error: Command could not be executed\n");
+    		else if(pid == 0) //Child Process
+    		{
+      			if(execvp(args[0], args) < 0)
+		  	{
+       				printf("Error: Command could not be executed\n");
 			}
 			exit(0);
-    }
-    else // Parent Process
-    {
-      waitpid(pid, &status, WUNTRACED);
-      input[0] = '\0';
-      k = 0;
-      *args = NULL;
-    }
-	fflush(stdin); 
-  while(1);
-
-  }
+   		}
+    		else // Parent Process
+    		{
+      			waitpid(pid, &status, WUNTRACED);
+      			input[0] = '\0';
+      			k = 0;
+      			*args = NULL;
+    		}
+		fflush(stdin); 
+  		while(1);
+ 	 }
 }
